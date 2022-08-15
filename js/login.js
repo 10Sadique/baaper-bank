4bf19ff7539501d05edd9d0598626c8eb90b5c3f
10Sadique <jafarsjahan@gmail.com>
2022-08-15 22:13:00 +0600

    email & pass auth added

---
+++ js/login.js
@@ -0,0 +1,19 @@
+const btnSubmit = document.getElementById('btn-submit')
+const userEmail = document.getElementById('user-email')
+const userPassword = document.getElementById('user-password')
+
+// Step 1: Add submit button event handler
+btnSubmit.addEventListener('click', () => {
+    // Step 2: Get email address value
+    let email = userEmail.value
+
+    // Step 3: Get password of user
+    let password = userPassword.value
+    
+    // Step 4: Verify email and password
+    if (email === 'jahan@email.com' && password === 'secret') {
+        console.log('Valid Account')
+    } else {
+        console.log('Invalid Account')
+    }
+})
